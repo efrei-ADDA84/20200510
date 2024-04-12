40da5ccd3bc77159c71a33716b0316ec254f9d18
nassimnady <nassim.nady@efrei.net>
2024-04-12 10:51:37 +0200

    update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 20200510
 
+TP1 : 
+
 Run docker : docker run -it --rm --name running_weather_app -e OPENWEATHER_API_KEY=your_api_key -e LATITUDE=your_latitude -e LONGITUDE=your_longitude my_weather_app
 
 
@@ -8,3 +10,15 @@ DockerHub link : https://hub.docker.com/repository/docker/nassim933/tp1/general
 Rapport: 
 
 J'ai utilisé python car c'est le langage que je précise le plus. A part pour la configuration de Docker et d'avoir le bon lien pour l'utilisation de l'API, j'ai pas rencontré d'autre difficultés . 
+
+
+TP2 : 
+
+link API : http://localhost:8000/get_weather
+
+body JSON : 
+{
+    "latitude" : "your_latitude",
+    "longitude" : "your_longitude",
+    "api_key" : "your API KEY"
+}
--- get_weather.py
+++ get_weather.py
@@ -1,13 +1,16 @@
 import os
 import requests
+from sanic import Sanic, response
+from sanic.exceptions import SanicException
+
+app = Sanic('get_weather')
 
 class WeatherWrapper:
     def __init__(self):
-        self.api_key = os.getenv('OPENWEATHER_API_KEY')
         self.base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-    def get_weather(self, lat, lon):
-        complete_url = self.base_url +"lat=" + str(lat)+  "&lon=" + str(lon)+"&appid=" + self.api_key
+    def get_weather(self, lat, lon, api_key):
+        complete_url = self.base_url +"lat=" + str(lat)+  "&lon=" + str(lon)+"&appid=" + str(api_key)
         response = requests.get(complete_url)
         data = response.json()
 
@@ -19,16 +22,36 @@ class WeatherWrapper:
             return main, weather_desc, country,city
         else:
             return None
+        
 
-if __name__ == "__main__":
-    lat = os.getenv('LATITUDE')
-    lon = os.getenv('LONGITUDE')
+
+@app.route("/get_weather", methods=['POST'])
+async def main(request):
+    data = request.json
+    if not data.get('latitude'):
+        raise SanicException("'latitude' are necessary", status_code = 500)
+    if not data.get('longitude'):
+        raise SanicException("'longitude' are necessary", status_code = 501)
+    if not data.get('api_key'):
+        raise SanicException("'api_key' are necessary", status_code = 502)
+    lat = data.get('latitude')
+    lon = data.get('longitude')
+    api_key = data.get('api_key')
     weather_wrapper = WeatherWrapper()
-    weather_info = weather_wrapper.get_weather(lat, lon)
+    weather_info = weather_wrapper.get_weather(lat, lon, api_key)
     if weather_info:
-        print("Country : ", weather_info[2])
-        print("City : ", weather_info[3])
-        print("Temperature : ", str(round(weather_info[0]["temp"] - 273.15, 2)))
-        print("Weather description : ", str(weather_info[1]))
+        temperature = round(weather_info[0]["temp"] - 273.15, 2)
+        weather_description = weather_info[1]
+        country = weather_info[2]
+        city = weather_info[3]
+        return response.json({
+            "country": country,
+            "city": city,
+            "temperature": temperature,
+            "weather_description": weather_description
+        })
     else:
-        print("Location not found!")
+        return response.json({"error": "Location not found!"}, status=404)
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
